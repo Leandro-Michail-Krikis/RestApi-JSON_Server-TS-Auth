19b59b78c66128cd1faae72a1faec90f32a96c9e
ACERLEANDRO\leo_d <leo1996.dasilva@gmail.com>
2022-11-10 23:19:12 -0300

    Ajustes na resposta dos metodos

--- server.ts
+++ server.ts
@@ -1,3 +1,5 @@
+//rotas para nao serem validadas coloque * para desabilitar a validação
+const rotasParaNaoValidar = ["/db", "/__rules"];
 //Criado dinamicamente
 import posts from "./dataBase/posts";
 //Criado estaticamente
@@ -15,14 +17,12 @@ const router = jsonServer.router({
   posts,
   comentarios,
 });
-//rotas para nao serem validadas
-const rotasParaNaoValidar = ["/db", "/__rules"]
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 //ednpoint para retornar token "http://localhost:3000/session" "{"email" : "admin","password" : "admin"}"
-server.post("/session", retornaToken);
+server.get("/session", retornaToken);
 //O token tem que ir nos headers como "Authorization"
 //Ele valida todos os endpoints exceto o http://localhost:3000 que e algo parecido com swagger
 server.use(validaUsuario);
@@ -32,31 +32,31 @@ server.listen(3000, () => {
   console.log("O servidor esta rodando: http://localhost:3000");
 });
 
-
 function retornaToken(req, res) {
-    const privateKey = fs.readFileSync('private.pem');
-    const { email, password } = req.body;
-    if(email == 'admin' && password == 'admin'){
-        const jwtBearerToken = jwt.sign({email: email}, privateKey, {
-            algorithm: 'RS256',
-            expiresIn: '1d'
-        })
-        return res.status(200).json({message: `${jwtBearerToken}`})
-    } else {
-        res.sendStatus(401);
-    }
+  const privateKey = fs.readFileSync("private.pem");
+  const { email, password } = req.body;
+  if (email == "admin" && password == "admin") {
+    const jwtBearerToken = jwt.sign({ email: email }, privateKey, {
+      algorithm: "RS256",
+      expiresIn: "1d",
+    });
+    return res.status(200).json({ message: `${jwtBearerToken}` });
+  } else {
+    return res.status(401).send({ message: "unauthorized" });
+  }
 }
 
 function validaUsuario(req, res, next) {
-    try {
-        if(rotasParaNaoValidar.indexOf(req.url) != -1) return next()
-        const publicKey = fs.readFileSync("public.pem");
-        const { authorization } = req.headers;
-        jwt.verify(authorization, publicKey, {
-          algorithm: "RS256",
-        });
-        next()
-      } catch (error) {
-        return res.status(401).send({message: "Você não tem autorização para este recurso."})
-      }
+  try {
+    if (rotasParaNaoValidar.indexOf("*") != -1) return next();
+    if (rotasParaNaoValidar.indexOf(req.url) != -1) return next();
+    const publicKey = fs.readFileSync("public.pem");
+    const { authorization } = req.headers;
+    jwt.verify(authorization, publicKey, {
+      algorithm: "RS256",
+    });
+    next();
+  } catch (error) {
+    return res.status(401).send({ message: "unauthorized" });
+  }
 }
